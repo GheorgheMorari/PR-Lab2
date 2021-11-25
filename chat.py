6a2cc4d55ae934adf35a917e05c5c146992bfe4c
Gheorghe Morari <53918731+GheorgheMorari@users.noreply.github.com>
2021-11-25 15:40:16 +0200

    add udp client

--- chat.py
+++ chat.py
@@ -4,11 +4,17 @@ import threading
 TCP_HOST = '127.0.0.1'
 TCP_PORT = 8000
 
+UDP_HOST = '127.0.0.1'
+UDP_PORT = 8001
+
 
 class Chat:
     def __init__(self):
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp_socket.bind((TCP_HOST, TCP_PORT))
+
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp_socket.bind((UDP_HOST, UDP_PORT))
 
     @staticmethod
     def get_response(string) -> str:
@@ -17,8 +23,10 @@ class Chat:
     def run(self):
         print("Starting server initialization")
         tcp_tread = threading.Thread(target=self.run_tcp)
+        udp_thread = threading.Thread(target=self.run_udp)
 
         tcp_tread.start()
+        udp_thread.start()
 
     def run_tcp(self):
         self.tcp_socket.listen()
@@ -36,6 +44,20 @@ class Chat:
                         send_data = Chat.get_response(string_data)
                         conn.sendall(bytes(send_data, encoding='utf8'))
                         print("Sent back via TCP:", send_data)
+
+    def run_udp(self):
+        print("UDP Server initialized")
+        while True:
+            data = self.udp_socket.recvfrom(1024)  # Maximum message length is 1024 bytes
+            if data:
+                # Converting the data from byte to string.
+                string_data = data[0].decode(encoding='utf-8')
+                print("Received via UDP:", string_data)
+                # Sending response
+                if string_data:
+                    send_data = Chat.get_response(string_data)
+                    self.udp_socket.sendto(bytes(send_data, encoding='utf8'), data[1])
+                    print("Sent back via UDP:", send_data)
 
 
 if __name__ == '__main__':
---
+++ udp_client.py
@@ -0,0 +1,18 @@
+import socket
+
+UDP_IP = '127.0.0.1'
+UDP_PORT = 8001
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World! FROM UDP"
+
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    data = s.sendto(bytes(MESSAGE, encoding='utf8'), (UDP_IP, UDP_PORT))
+    response = s.recvfrom(BUFFER_SIZE)[0]
+
+    print("Bytes sent:", data)
+    response_str = response.decode("utf-8")
+    print("Sent:", str(MESSAGE))
+    print("Received:", response_str)
+    s.close()
